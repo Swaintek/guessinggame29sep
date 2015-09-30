2f56f5909d4795dbffcb44c6bf23444a68e26422
David Swaintek <dswaintek@gmail.com>
2015-09-30 12:10:04 -0700

    First commit

---
+++ game.js
@@ -0,0 +1,11 @@
+var question1 = prompt("Is Seattle a great music town?");
+var question2 = prompt("Is Kurt Cobain dead?");
+var question3 = prompt("Does Eddie Vedder love flannel?");
+
+console.log('Question 1 response: ' + question1);
+console.log('Question 2 response: ' + question2);
+console.log('Question 3 response: ' + question3);
+
+alert('Your answer to Question 1 was ' + question1);
+alert('Your answer to Question 2 was ' + question2);
+alert('Your answer to Question 3 was ' + question3);
